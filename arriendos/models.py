f6cf1480b333713c56d29e029cae3beaa7cd75b7
andrea gutierrez <andreasiciliag@gmail.com>
2024-05-07 13:09:58 -0400

    muestra comunas en listado de inmuebles

--- arriendos/models.py
+++ arriendos/models.py
@@ -2,59 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-# Arreglo de tuplas para las comunas de la Región Metropolitana de Santiago
-# Arreglo de tuplas para las comunas de la Región Metropolitana de Santiago
-COMUNAS_REGION_METROPOLITANA = [
-    ("alhue", "Alhué"),
-    ("buin", "Buin"),
-    ("calera_de_tango", "Calera de Tango"),
-    ("cerrillos", "Cerrillos"),
-    ("cerro_navia", "Cerro Navia"),
-    ("colina", "Colina"),
-    ("conchali", "Conchalí"),
-    ("curacavi", "Curacaví"),
-    ("el_bosque", "El Bosque"),
-    ("el_monte", "El Monte"),
-    ("estacion_central", "Estación Central"),
-    ("huechuraba", "Huechuraba"),
-    ("independencia", "Independencia"),
-    ("isla_de_maipo", "Isla de Maipo"),
-    ("la_cisterna", "La Cisterna"),
-    ("la_florida", "La Florida"),
-    ("la_granja", "La Granja"),
-    ("la_pintana", "La Pintana"),
-    ("la_reina", "La Reina"),
-    ("lampa", "Lampa"),
-    ("las_condes", "Las Condes"),
-    ("lo_barnechea", "Lo Barnechea"),
-    ("lo_espejo", "Lo Espejo"),
-    ("lo_prado", "Lo Prado"),
-    ("macul", "Macul"),
-    ("maipu", "Maipú"),
-    ("melipilla", "Melipilla"),
-    ("nunoa", "Ñuñoa"),
-    ("padre_hurtado", "Padre Hurtado"),
-    ("penaflor", "Peñaflor"),
-    ("penalolen", "Peñalolén"),
-    ("pirque", "Pirque"),
-    ("puente_alto", "Puente Alto"),
-    ("providencia", "Providencia"),
-    ("pudahuel", "Pudahuel"),
-    ("quilicura", "Quilicura"),
-    ("quinta_normal", "Quinta Normal"),
-    ("recoleta", "Recoleta"),
-    ("renca", "Renca"),
-    ("san_bernardo", "San Bernardo"),
-    ("san_joaquin", "San Joaquín"),
-    ("san_jose_de_maipo", "San José de Maipo"),
-    ("san_miguel", "San Miguel"),
-    ("san_pedro", "San Pedro"),
-    ("san_ramon", "San Ramón"),
-    ("santiago", "Santiago"),
-    ("talagante", "Talagante"),
-    ("til_til", "Til Til"),
-    ("vitacura", "Vitacura"),
-]
+
+class Region(models.Model):
+    nombre = models.CharField(max_length=100)
+    def __str__(self):
+        return self.nombre
+    
+    
+class Comuna(models.Model):
+    nombre = models.CharField(max_length=100)
+    region = models.ForeignKey(Region, related_name='comunas', on_delete=models.CASCADE)
+    def __str__(self):
+        return self.nombre
 
 
 class Usuario(User):
@@ -86,7 +45,7 @@ class Inmueble(models.Model):
     descripcion=models.CharField(max_length=50)
     imagen = models.ImageField(upload_to='img/inmuebles/')
     precio=models.DecimalField(max_digits=10, decimal_places=0)
-    comuna = models.CharField(max_length=50, choices=COMUNAS_REGION_METROPOLITANA)
+    comuna = models.ForeignKey(Comuna, related_name='inmuebles', on_delete=models.CASCADE)
     disponible=models.BooleanField(default=True)
     m2_construidos = models.DecimalField(max_digits=10, decimal_places=2)
     m2_terreno = models.DecimalField(max_digits=10, decimal_places=2)
